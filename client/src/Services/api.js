6e2b2fc4253a91dd3c2f21c516aee6ebb9cd98b0
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-10 20:51:00 +0530

    initial commites

---
+++ client/src/Services/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const URL = "http://localhost:8000";
+
+export const uploadFile = async (data) => {
+  try {
+    let response = await axios.post(`${URL}/upload`, data);
+    return response.data;
+  } catch (error) {
+    console.log("Error while calling the api", error.message);
+  }
+};
