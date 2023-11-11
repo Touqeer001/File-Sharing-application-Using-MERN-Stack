d32ab2d3314ab94a06bb0bfa6c8972f998178168
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-11 21:05:16 +0530

    updated app.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import router from "./routes/routes.js";
+
 const app = express();
 
 const PORT = 8000;
+
+app.use("/", router);
 
 app.listen(PORT, () => console.log(`Server is Running on Port ${PORT}`));
