e70aca063d5a95e70f4f11fcbd09b06329bb1c00
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-11 21:12:27 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes/routes.js";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 8000;
 
+app.use(cors());
 app.use("/", router);
 
 app.listen(PORT, () => console.log(`Server is Running on Port ${PORT}`));
