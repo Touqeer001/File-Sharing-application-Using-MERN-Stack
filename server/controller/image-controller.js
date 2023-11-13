443e2e8091f9bdd302534eb0552fd9c581ed1c29
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-13 20:36:59 +0530

    update

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,3 +1,16 @@
+import File from "../model/file.js";
+
 export const uplaodImage = async (request, response) => {
-  return response.status(200).json({ msg: "hello this is upload controller" });
+  console.log(request);
+  const fileObj = {
+    path: request.file.path,
+    name: request.file.originalname,
+  };
+  try {
+    const file = await File.create(fileObj);
+    console.log(file);
+    response.status(200).json({});
+  } catch (error) {
+    console.error(error.message);
+  }
 };
