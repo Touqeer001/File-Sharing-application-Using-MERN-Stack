e05d2322bb85843fbd764c359eff6d49deaa2e72
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-13 21:25:11 +0530

    update

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { uplaodImage } from "../controller/image-controller.js";
+import { downloadImage, uplaodImage } from "../controller/image-controller.js";
 import upload from "../utils/upload.js";
 
 const router = express.Router();
 router.post("/upload", upload.single("file"), uplaodImage);
+
+//for downloading the file
+router.get("/file/:fileId", downloadImage);
 
 export default router;
