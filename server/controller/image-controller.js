884c0b165870a40348bdde080097236ccb4a56e8
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-13 21:25:06 +0530

    update

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -9,8 +9,25 @@ export const uplaodImage = async (request, response) => {
   try {
     const file = await File.create(fileObj);
     console.log(file);
-    response.status(200).json({});
+    response
+      .status(200)
+      .json({ path: `http://localhost:8000/file/${file._id}` }); //examplehttps://www.branchcms.com/learn/docs/overview/uploading-files-images/image-upload
   } catch (error) {
     console.error(error.message);
+    response.status(500).json({ error: error.message });
+  }
+};
+
+//url me ? mark ke baad jo aata h use query kehte h or slash/ ke baad jo aata h use Params kehte h....
+
+export const downloadImage = async (request, response) => {
+  try {
+    const file = await File.findById(request.params.fileId);
+    file.downloadContent++;
+    await file.save();
+    response.download(file.path, file.name);
+  } catch (error) {
+    console.error(error.message);
+    return response.status(500).json({ error: error.message });
   }
 };
